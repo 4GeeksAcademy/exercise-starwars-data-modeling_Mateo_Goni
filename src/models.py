49f1ade7d29c613ecbef342a1c5895b702a530fb
mateo1627 <140858704+mateo1627@users.noreply.github.com>
2024-07-24 13:59:22 +0000

    Project finished

--- src/models.py
+++ src/models.py
@@ -27,12 +27,14 @@ class Characters(Base):
     hair_color = Column(String(250), nullable=False)
     eyes_color = Column(String(250), nullable=False)
     height = Column(Integer, nullable=False)
+   
 
 class Vehicles(Base):
     __tablename__ = 'vehicles'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     model = Column(String(250), nullable=False)
+    
     
 class Planets(Base):
     __tablename__ = 'planets'
@@ -41,18 +43,16 @@ class Planets(Base):
     age = Column(Integer, nullable=False)
     climate = Column(String(250), nullable=False)
     population = Column(String(250), nullable=False)
-    
-class Favorites(Base):
-    __tablename__ = 'favorites'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship('Users', back_populates='favorites')
+   
+
 class Favorites_planets(Base):
     __tablename__ = 'favorites_planets'
     id= Column(Integer, primary_key=True)
     planets_id = Column(Integer, ForeignKey('planets.id'))
     favorites = relationship('Favorites', back_populates='favorites_planets')
     planets = relationship('Planets')
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('Users', back_populates='favorites')
 
 class Favorites_characters(Base):
     __tablename__ = 'favorites_characters'
@@ -60,6 +60,8 @@ class Favorites_characters(Base):
     characters_id = Column(Integer, ForeignKey('characters.id'))
     favorites = relationship('Favorites', back_populates='favorites_characters')
     characters = relationship('Characters')
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('Users', back_populates='favorites')
 
 class Favorites_vehicles(Base):
     __tablename__ = 'favorites_vehicles'
@@ -67,6 +69,8 @@ class Favorites_vehicles(Base):
     vehicles_id = Column(Integer, ForeignKey('vehicles.id'))
     favorites = relationship('Favorites', back_populates='favorites_vehicles')
     vehicles = relationship('Vehicles')
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('Users', back_populates='favorites')
 
     def to_dict(self):
         return {}
